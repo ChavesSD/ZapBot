3835013c754aaeb3fc40a31168b55291b8186d11
Deyvison de Souza Chaves <deyvisonchaves@gmail.com>
2025-05-22 15:33:31 -0300

    \Remover diretórios não utilizados (wppconnect-server, teste-node, whatsapp-api)"

--- teste-node/index.js
+++
@@ -1,5 +0,0 @@
-import chalk from 'chalk';
-
-console.log(chalk.green('Teste do Node.js funcionando!'));
-console.log(chalk.blue(`Versão do Node.js: ${process.version}`));
-console.log(chalk.yellow(`Plataforma: ${process.platform}`)); 
